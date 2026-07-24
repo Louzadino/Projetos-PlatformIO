7b3a0ab9b00374c779563f2938c707e74dd11f55
Guilherme <guilhermelouzada852@gmail.com>
2026-07-24 17:19:20 -0300

    Correcoes mao robotica para rodar no ubuntu

---
+++ mao_robotica/src/hardware/main.cpp
@@ -0,0 +1,88 @@
+//Codigo que recebe quais dedos da mao mimica devem fechar ou abrir conforme o codigo de visao computacional observou
+
+#include <Arduino.h>
+#include <Servo.h>
+
+Servo servoPolegar;
+Servo servoIndicador;
+Servo servoMeio;
+Servo servoAnelar;
+Servo servoMindinho;
+
+#define qtdValoresRecebidos 5
+#define digitosValorRecebido 1
+
+int valoresRecebidos[qtdValoresRecebidos];
+int tamanhoString = qtdValoresRecebidos * digitosValorRecebido +1;    //$00000
+int contador = 0;
+bool contadorStart = false;
+String stringRecebida;
+
+void setup() {
+  Serial.begin(9600);
+  servoPolegar.attach(7);
+  servoIndicador.attach(9);
+  servoMeio.attach(11);
+  servoAnelar.attach(8);
+  servoMindinho.attach(10);
+}
+
+void data() {
+  while(Serial.available()) {
+    char c = Serial.read();
+
+    if (c == '$') {
+      contadorStart = true;
+    }
+
+    if (contadorStart) {
+      if (contador < tamanhoString) {
+        stringRecebida = String(stringRecebida+c);
+        contador++;
+      }
+
+      if (contador >= tamanhoString) {
+        for(int i=0; i<qtdValoresRecebidos; i++) {
+          int num = (i*digitosValorRecebido)+1;
+          valoresRecebidos[i] = stringRecebida.substring(num, num + digitosValorRecebido).toInt();
+        }
+
+        stringRecebida = "";
+        contador = 0;
+        contadorStart = false;
+      }
+    }
+  }
+}
+
+int associaAngulo (int detectado) {
+  int angulo;
+
+  if(detectado == 0) angulo = 180;
+  else if(detectado == 1) angulo = 135;
+  else if (detectado == 2) angulo = 90;
+  else if (detectado == 3) angulo = 45;
+  else angulo = 0;
+
+  return angulo;
+}
+
+void loop() {
+  data();
+  int angulo;
+  
+  angulo = associaAngulo(valoresRecebidos[0]);
+  servoPolegar.write(angulo);
+
+  angulo = associaAngulo(valoresRecebidos[1]);
+  servoIndicador.write(angulo);
+
+  angulo = associaAngulo(valoresRecebidos[2]);
+  servoMeio.write(angulo);
+
+  angulo = associaAngulo(valoresRecebidos[3]);
+  servoAnelar.write(angulo);
+
+  angulo = associaAngulo(valoresRecebidos[4]);
+  servoMindinho.write(angulo);
+}
---
+++ mao_robotica/src/software/classes_menu/MaoMimica.py
@@ -0,0 +1,106 @@
+
+import tkinter as tk
+import cv2 as openCv
+import time
+import serial
+import mediapipe as mp
+from tkinter import messagebox
+
+from classes_mao.Mao import Mao
+
+class MaoMimica:
+    def __init__(self):
+        self.porta_serial = "/dev/ttyACM0"
+        self.baud_rate = 9600
+        self.estadosDedosAnterior = [-1, -1, -1, -1, -1]
+
+    
+    def verificaEstadosMao(self, estadosDedos):
+        if (((self.estadosDedosAnterior[0] == 0) or (self.estadosDedosAnterior[0] == 1)) and 
+            ((self.estadosDedosAnterior[1] == 0) or (self.estadosDedosAnterior[1] == 1))) :
+            print("entrou aqui")
+            if (estadosDedos[0] != 0 and estadosDedos[0] != 1):
+                estadosDedos[1] = 2
+                print("teve que abrir o indicador ate o meio termo antes de abrir o polegar")
+            elif (estadosDedos[1] != 0 and estadosDedos[1] != 1):
+                estadosDedos[0] = 2
+                print("teve que abrir o polegar ate o meio termo antes de abrir o indicador")
+
+    def imitar(self):
+        messagebox.showinfo("Instrução", "Movimente sua mão para enviar comandos ao Arduino.\nPressione 'E' para encerrar.")
+        arduino = serial.Serial(self.porta_serial, self.baud_rate)
+        time.sleep(2) 
+
+        camera = openCv.VideoCapture(0, openCv.CAP_V4L2)
+
+        # Força o formato de compressão MJPG (resolve o gargalo da USB)
+        camera.set(openCv.CAP_PROP_FOURCC, openCv.VideoWriter_fourcc('M', 'J', 'P', 'G'))
+
+        # Define a resolução
+        camera.set(3,640)
+        camera.set(4,480)
+
+        # Limita o buffer para pegar sempre o frame mais recente
+        camera.set(openCv.CAP_PROP_BUFFERSIZE, 1)
+
+        # Dá o tempo necessário para o hardware ligar
+        time.sleep(2)
+
+        hands = mp.solutions.hands
+        Hands = hands.Hands(max_num_hands=1)
+        mediapipeDraw = mp.solutions.drawing_utils
+
+        while True:
+            success, imagem = camera.read()
+
+            if not success or imagem is None:
+                print("Frame vazio ou falha na leitura. Tentando novamente...")
+                continue # Ignora o erro, pula o resto do bloco e tenta capturar o próximo frame
+
+            frameRGB = openCv.cvtColor(imagem, openCv.COLOR_BGR2RGB)
+            resultados = Hands.process(frameRGB)
+            pontosMao = resultados.multi_hand_landmarks
+            altura, largura, _ = imagem.shape
+            pontos = []
+            if pontosMao:
+                for points in pontosMao:
+                    mediapipeDraw.draw_landmarks(imagem, points, hands.HAND_CONNECTIONS)
+                    for id, coordenada in enumerate(points.landmark):
+                        coordenadaX, coordenadaY = int(coordenada.x * largura), int(coordenada.y * altura)
+                        openCv.circle(imagem, (coordenadaX, coordenadaY), 4, (255,0,0), -1)
+                        pontos.append((coordenadaX, coordenadaY))
+
+                    if pontos:
+                        mao = Mao(pontos)
+                        estadosDedos = mao.analisarDedosMao()
+                        print(self.estadosDedosAnterior)
+
+                        if self.estadosDedosAnterior[0] != -1:
+                            # Ele verifica se eh o primeiro movimento que a mão esta fazendo, se nao for...
+                            # ele chama uma funcao para verificar se os vetores indicam que o usuário 
+                            # está tentando levantar um dedo que está "preso/travado" por outro.
+                            # Por exemplo, quando a pessoa quer levantar o indicador mas o polegar também está abaixado, e vice-versa
+                            # (aconteceu muito na amostra de pic e toda vez tem que abrir o antebraço pra acertar o fio)
+                            # Ai no caso essa função verificaria e abriria um pouco o dedo que está travando
+                            self.verificaEstadosMao(estadosDedos)
+
+                        mensagem = f"${''.join(map(str, estadosDedos))}"
+                        print(f"Enviando para Arduino: {mensagem}")
+
+                        # Enviando para o Arduino via Serial
+                        arduino.write(mensagem.encode())
+                        self.estadosDedosAnterior = estadosDedos.copy()
+
+            openCv.imshow('Imagem', imagem)
+            key = openCv.waitKey(1) & 0xFF
+
+            if key == ord('e') or key == ord('E') :
+                estadosDedos = [4, 4, 4, 4, 4]
+                mensagem = f"${''.join(map(str, estadosDedos))}"
+
+                # Enviando para o Arduino via Serial
+                arduino.write(mensagem.encode())
+                break
+        camera.release()
+        openCv.destroyAllWindows()
+        messagebox.showinfo("Finalizado", "Movimentos imitados com sucesso!")
